fa1f3f9f24179b5be64744d766fbd66c3ed658be
fire8327 <fire83274@gmail.com>
2025-08-01 09:13:21 +0300

    create index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,48 @@
 <template>
-    <div>
-        Главная страница
-    </div>
+    <section class="pt-9 grid-container bg-white">
+        <div class="flex flex-col gap-1">
+            <div class="flex items-center gap-4">
+                <p class="text-[32px] leading-[100%] font-medium">Здравствуйте, Евгений!</p>
+                <img src="/images/index/hand.png" alt="">
+            </div>
+            <div class="flex items-center justify-between">
+                <p class="text-base leading-[100%] text-[#ADADAD]">Давайте посмотрим ваши данные по серверам</p>
+                <label class="flex items-center gap-3 py-2 px-3.5 cursor-pointer group">
+                    <input type="checkbox" class="sr-only">
+                    <span class="w-[18px] h-[18px] rounded-[3px] bg-[#EFEFEF] border border-[#E5E5E5] flex items-center justify-center transition-all duration-500 [.group:has(input:checked)_&]:bg-[#136BFB] [.group:has(input:checked)_&]:border-[#136BFB]">
+                        <img src="/images/index/check.png" alt="" class="transition-all duration-500 opacity-0 group-hover:opacity-100 [.group:has(input:checked)_&]:opacity-100">
+                    </span>
+                    <span class="text-xs leading-[100%]">Показать только активные</span>
+                </label>
+            </div>
+            <div class="flex items-center mt-6">
+                <div class="flex items-center justify-between py-4 px-5 md:w-[244px] rounded-t-[20px] bg-[#F7F7F7] border border-[#F7F7F7]">
+                    <div class="flex items-center gap-3.5">
+                        <span class="text-xl font-semibold leading-[100%]">2</span>
+                        <span class="text-base font-semibold leading-[100%]">Все сервера</span>
+                    </div>
+                    <div class="flex items-center gap-1">
+                        <img src="/images/index/server_s.png" alt="">
+                        <img src="/images/index/server_a.png" alt="">
+                    </div>
+                </div>
+                <div class="flex items-center justify-between py-4 px-5 md:w-[244px] rounded-t-[20px] border border-[#EFEFEF]">
+                    <div class="flex items-center gap-3.5">
+                        <span class="text-xl font-semibold leading-[100%]">1</span>
+                        <span class="text-base font-semibold leading-[100%]">Revit Server</span>
+                    </div>
+                    <img src="/images/index/server_s.png" alt="">
+                </div>
+                <div class="flex items-center justify-between py-4 px-5 md:w-[244px] rounded-t-[20px] border border-[#EFEFEF]">
+                    <div class="flex items-center gap-3.5">
+                        <span class="text-xl font-semibold leading-[100%]">1</span>
+                        <span class="text-base font-semibold leading-[100%]">Revit Accelerator</span>
+                    </div>
+                    <img src="/images/index/server_a.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup>
